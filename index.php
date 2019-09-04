d15294dde140894d3ba3e601e57b1e43b151be37
sully95 <54905055+sully95@users.noreply.github.com>
2019-09-04 12:50:04 -0400

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,30 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.js"></script>
+    <style>
+#customers {
+  font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+#customers td, #customers th {
+  border: 1px solid #ddd;
+  padding: 8px;
+}
+
+#customers tr:nth-child(even){background-color: #f2f2f2;}
+
+#customers tr:hover {background-color: #ddd;}
+
+#customers th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: #4CAF50;
+  color: white;
+}
+</style>
     <style type="text/css">
         .wrapper{
             width: 650px;
@@ -17,50 +41,54 @@
         table tr td:last-child a{
             margin-right: 15px;
         }
-  <style>
-table {
-  font-family: arial, sans-serif;
-  border-collapse: collapse;
-  width: 100%;
-}
-
-td, th {
-  border: 1px solid #dddddd;
-  text-align: left;
-  padding: 8px;
-}
-
-tr:nth-child(even) {
-  background-color: #dddddd;
-}
-</style>
+    </style>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $('[data-toggle="tooltip"]').tooltip();   
+        });
+    </script>
 </head>
 <body>
-
-<h2>Carahsoft Technical Team</h2>
-
-<table>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="page-header clearfix">
+                        <h2 class="pull-left">Employees Details</h2>
+                        <a href="create.php" class="btn btn-success pull-right">Add New Employee</a>
+                    </div>
+         
+  <table id="customers">
   <tr>
-    <th>Contact</th>
+    <th>Employee</th>
     <th>Phone</th>
+    <th>Specialization</th>
   </tr>
   <tr>
     <td>Alex Sullivan</td>
     <td>703-889-9776</td>
+    <td>vRealize and VMC on AWS</td>
   </tr>
   <tr>
     <td>Ethan Palmer</td>
     <td>703-230-7542</td>
+    <td>NSX and HCI (vSAN)</td>
   </tr>
   <tr>
-    <td>Nick Wagner</td>
+    <td>Nick Wager</td>
     <td>703-581-6615</td>
+    <td>Horizon and WorkspaceONE</td>
   </tr>
   <tr>
-    <td>Olivia Yarosh</td>
-    <td>571-662-4645</td>
+    <td>John Sneed</td>
+    <td>703-921-4192</td>
+    <td>AWS Native and VMC on AWS</td>
   </tr>
+  
 </table>
 
+</body>                  
+
+   
 </body>
 </html>
